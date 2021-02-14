2dd6f5cb46364e48bfb8c126e5df31c04c5aed1e
Horacio Belardita <horaciobelardita@gmail.com>
2021-02-13 23:19:41 -0300

    resolve exercises

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,19 +1,54 @@
-export function multiplicarPor10(array) {}
+export function multiplicarPor10(array) {
+    return array.map(item => item * 10)
+}
 
-export function soloPares(array) {}
+export function soloPares(array) {
+    return array.filter(item => item % 2 === 0)
+}
 
-export function moverALaDerecha(array) {}
+export function moverALaDerecha(array) {
+    const lastIndex = array.length - 1
+    return [
+        array[lastIndex],
+        ...array.slice(0,lastIndex)
+    ]
+}
 
-export function soloUnaPalabra(array) {}
+export function soloUnaPalabra(array) {
+    return array.filter(word => !word.includes(' '))
+}
 
-export function soloArraysPositivos(array) {}
+export function soloArraysPositivos(array) {
+   return [...array].filter(nested => nested.every(item => item > 0) )
 
-export function soloVocales(array) {}
+}
 
-export function todasVocalesIguales(array) {}
+export function soloVocales(array) {
+    return array.map(str => str.replace(/[^aeiou]/g, ""))
+}
 
-export function duplicarMatriz(array) {}
+export function todasVocalesIguales(array) {
+    return array.filter(word => {
+        const firstVowelInWord = [...word].find(char => char.match(/[aeiou]/))
+        const vowelsInWord = [...word].filter(char => char.match(/[aeiou]/))
+        return vowelsInWord.every(char => char === firstVowelInWord)
+    })
+}
 
-export function mayoresDeEdadValidados(array) {}
+export function duplicarMatriz(array) {
+   return [...array].map(nested => nested.map(item => item * 2))
+}
 
-export function transformarObjetoEnArray(objeto) {}
+export function mayoresDeEdadValidados(array) {
+    return [...array].map(obj => ({
+        ...obj,
+        mayor: obj.edad > 18
+    }))
+}
+
+export function transformarObjetoEnArray(objeto) {
+    return Object.keys(objeto).map(key => ({
+        id: key,
+        ...objeto[key]
+    }))
+}
